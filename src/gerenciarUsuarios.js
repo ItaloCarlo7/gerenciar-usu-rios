167dcd1b328863feff1da8452c2761c75723ecd0
Italo <italocarlo43@gmail.com>
2025-09-10 11:04:37 -0300

    atualização da aula 4, adicionar novo nome na lista

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,14 @@
+const usuarios =['Matheus', 'Marcos' , 'Lucas', 'João'];
+
+function retornarUsuarios(usuarios){
+    return usuarios;
+
+};
+function adicionarNovoUsusario() {
+    usuarios.push(nome);
+}
+
+module.exports = {
+    retornarUsuarios,
+    adicionarNovoUsusario
+}
---
+++ test/gerenciarUsuarios.test.js
@@ -0,0 +1,14 @@
+const { gerenciarUsuarios } = require('../src/gerenciarUsuarios');
+const { expect } = require('chai');
+
+describe('Testar as funções de gestão de Usuários', function () {
+    it('Validar que posso adicionar um novo nome de usuário na lista',function () {
+        // 1. Adicionar um novo nome na lista de usuários
+
+        // 2. Retornar a lista de usuários na caixa lista de usuários
+
+        // 3. Verificar se o novo nome foi adicionado no fim da lista de usuários
+
+        // 4. Comparar se o ultimo nome adicionado está igual ao nome adicionado
+    })
+})
